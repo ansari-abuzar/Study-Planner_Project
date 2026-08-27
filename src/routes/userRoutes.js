1fc655524aeae6945f9f1b2e7dfb93cf28766f8c
Abuzar Ansari <abuzar.ansari.0207@gmail.com>
2026-08-27 11:19:18 +0530

    Used App Error to show errors.

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,16 +1,21 @@
 import express from "express";
 import authMiddleware from "../middleware/authMiddleware.js";
-import { getProfile, updateProfile, deleteProfile } from "../controllers/userController.js";
+import {
+  getProfile,
+  updateProfile,
+  deleteProfile,
+} from "../controllers/userController.js";
+import asyncHandler from "../utils/asyncHandler.js";
 
 const router = express.Router();
 
 // Route to get the user profile
-router.get("/profile", authMiddleware, getProfile)
+router.get("/profile", authMiddleware, asyncHandler(getProfile));
 
 // Updating existing records of a user
-router.put("/profile", authMiddleware, updateProfile)
+router.put("/profile", authMiddleware, asyncHandler(updateProfile));
 
 // Delete user profile.
-router.delete("/profile", authMiddleware, deleteProfile)
+router.delete("/profile", authMiddleware, asyncHandler(deleteProfile));
 
 export default router;
