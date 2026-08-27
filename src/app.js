2898ba36fb9976e38297320802099c6ced5d0119
Abuzar Ansari <abuzar.ansari.0207@gmail.com>
2026-08-27 11:16:31 +0530

    Added the error handler.

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
 import express from "express";
 import cors from "cors";
 import authRoutes from "../src/routes/authRoutes.js";
-import userRoutes from "./routes/userRoutes.js"
+import userRoutes from "./routes/userRoutes.js";
+import { errorHandler } from "./middleware/errorMiddleware.js";
 
 const app = express();
 
@@ -10,5 +11,7 @@ app.use(express.json());
 
 app.use("/auth", authRoutes);
 app.use("/user", userRoutes);
+
+app.use(errorHandler);
 
 export default app;
