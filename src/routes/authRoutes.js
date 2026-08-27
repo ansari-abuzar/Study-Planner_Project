b0113ac185b71c79f9fb95e60b8b56046cef9a6d
Abuzar Ansari <abuzar.ansari.0207@gmail.com>
2026-08-27 11:17:34 +0530

    Implemented the Async Handler to catch errors.

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,12 +1,13 @@
 import express from "express";
 import { loginUser, registerUser } from "../controllers/authController.js";
+import asyncHandler from "../utils/asyncHandler.js";
 
 const router = express.Router();
 
 // Registering a user
-router.post("/register", registerUser);
+router.post("/register", asyncHandler(registerUser));
 
 // Login Feature
-router.post("/login",loginUser)
+router.post("/login", asyncHandler(loginUser));
 
 export default router;
