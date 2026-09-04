08948d96ca74b2f632d99fcb6b191012145a48d3
Abuzar Ansari <abuzar.ansari.0207@gmail.com>
2026-09-04 11:29:39 +0530

    Added basic Register Component

--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -4,7 +4,7 @@ function Register() {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    const responce = await fetch("http://localhost:5000/auth/register", {
+    const response = await fetch("http://localhost:5000/auth/register", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -16,8 +16,7 @@ function Register() {
       }),
     });
 
-    const data = await responce.json();
-    console.log(data);
+    console.log(response.status);
   };
 
   const [name, setName] = useState("");
